6e3e70ec1fd9eb560667146a44616db48ae3000a
bill <wcstrickland1@gmail.com>
2022-05-28 14:25:46 -0500

    added conditional port and new routes

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"log"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
@@ -22,15 +23,46 @@ func main() {
 	app := fiber.New()
 	app.Use(recover.New())
 
-	app.Get("/tz/:zip", func(c *fiber.Ctx) error {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("/tz/zip:zip\treturns a timezone from a given zipcode\nEXAMPLE: /tz/zip/35210\n/tz/area/:area\treturns a timezone from a given area code\n/tz/state/:state\treturns a timezone from a given state\nEXAMPLE: /tz/state/AL\n\n/download\tdownloads the underlying sqlite database file")
+	})
+
+	app.Get("/tz/zip/:zip", func(c *fiber.Ctx) error {
 		zip := c.Params("zip")
 		var tz string
 		row := db.QueryRow("select timezone from tz where zip = ?", zip)
 		if err := row.Scan(&tz); err != nil {
-			return c.SendString(err.Error())
+			return c.Status(404).SendString("No Dice")
 		}
 		return c.SendString(tz)
 	})
 
-	log.Fatal(app.Listen(":3000"))
+	app.Get("/tz/area/:area", func(c *fiber.Ctx) error {
+		area := c.Params("area")
+		var tz string
+		row := db.QueryRow("select timezone from tz where area_codes = ?", area)
+		if err := row.Scan(&tz); err != nil {
+			return c.Status(404).SendString("No Dice")
+		}
+		return c.SendString(tz)
+	})
+
+	app.Get("/tz/state/:state", func(c *fiber.Ctx) error {
+		state := c.Params("state")
+		var tz string
+		row := db.QueryRow("select timezone from tz where state = ?", state)
+		if err := row.Scan(&tz); err != nil {
+			return c.Status(404).SendString("No Dice")
+		}
+		return c.SendString(tz)
+	})
+
+	app.Get("/download", func(c *fiber.Ctx) error {
+		return c.Download("./tz.db")
+	})
+	port := os.Getenv("PORT")
+	if os.Getenv("PORT") == "" {
+		port = "3000"
+	}
+	log.Fatal(app.Listen(":" + port))
 }
